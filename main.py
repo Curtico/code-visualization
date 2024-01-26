671b9cf5875ef5d26972079d9f8c1b1684c356a1
Joshua Hartzfeld <102620766+JoshuaHartz@users.noreply.github.com>
2024-01-26 16:40:29 -0500

    Foundation change one
    Adding template foundation from https://github.com/gciftci/PyQT-Template?tab=readme-ov-file

---
+++ main.py
@@ -0,0 +1,9 @@
+''' main.py '''
+from app import init
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(init.run())
+
+
+# docs : https://github.com/gciftci/PyQT-Template?tab=readme-ov-file
